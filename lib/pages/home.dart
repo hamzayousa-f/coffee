9e16f1b18ae76c94618183c6483daffc4b3eb86f
Hamza <hamzahunt65@gmail.com>
2023-04-11 00:33:35 +0500

    3nd Commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,9 @@
 import 'package:coffee/components/nav_bar.dart';
+import 'package:coffee/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +13,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+//pages
+  final List<Widget> _pages = [
+    ShopPage(),
+    CartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
